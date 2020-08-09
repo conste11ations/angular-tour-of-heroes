4b71a9bbd5e65a13f33840b452832d40eb38f5e7
Rachel Kat <rachelkat@gmail.com>
2020-08-09 19:07:33 -0400

    updating the messenger service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  messages: string[] = [];
 
-  constructor() { }
+  add(message: string): void {
+    this.messages.push(message);
+  }
+
+  clear(): void {
+    this.messages = [];
+  }
 }
