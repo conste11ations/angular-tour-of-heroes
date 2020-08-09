8cf7d36ead731515f8d09fc5c6ef09c24ec1f1ca
Rachel Kat <rachelkat@gmail.com>
2020-08-09 19:12:57 -0400

    Angular only binds to public component properties

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+  // TODO: send the message _after_ fetching the heroes
+  this.messageService.add('HeroService: fetched heroes');
+  return of(HEROES);
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
