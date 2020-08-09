f281376062b885569a688610ca6a9e8b04f9d42b
Rachel Kat <rachelkat@gmail.com>
2020-08-09 01:26:15 -0400

    event binding syntax onSelect and ngIf

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,9 +13,13 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() {
-   }
+  }
 
   ngOnInit(): void {
   }
