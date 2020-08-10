fa143b45b1d9871367f8305e97344f0a308a7cc3
Rachel Kat <rachelkat@gmail.com>
2020-08-10 02:56:45 -0400

    add save for hero details

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
@@ -27,6 +30,14 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
