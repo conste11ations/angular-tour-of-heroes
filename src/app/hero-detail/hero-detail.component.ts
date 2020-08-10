49b716017da0959f26648f99b8aa8f8f73d218e5
Rachel Kat <rachelkat@gmail.com>
2020-08-09 23:20:27 -0400

    hero detail css

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
@@ -25,5 +25,9 @@ export class HeroDetailComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
