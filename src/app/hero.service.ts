e83534eeecbf28a76633b160c06e6a49d0747aef
Rachel Kat <rachelkat@gmail.com>
2020-08-09 20:13:13 -0400

    hero service getHero()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,10 +8,16 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+
   getHeroes(): Observable<Hero[]> {
   // TODO: send the message _after_ fetching the heroes
   this.messageService.add('HeroService: fetched heroes');
   return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
   constructor(private messageService: MessageService) { }
 }
