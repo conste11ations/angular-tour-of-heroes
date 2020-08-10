0914adc19466563100ef3f4d4f91ec95287cc328
Rachel Kat <rachelkat@gmail.com>
2020-08-10 02:45:46 -0400

    update methods to use web api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,10 +21,13 @@ export class HeroService {
         catchError(this.handleError<Hero[]>('getHeroes', []))
       );
   }
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   constructor(
